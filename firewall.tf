09249de1fdf43615bfee46c9eebbe845cacd8213
jehad215 <gehadmosa51@gmail.com>
2023-01-28 14:27:24 +0200

    firewall

---
+++ firewall.tf
@@ -0,0 +1,10 @@
+resource "google_compute_firewall" "allow" {
+  name = "allow"
+  network = google_compute_network.vpc-final.id
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
